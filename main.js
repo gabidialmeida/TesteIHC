9e5819833d90f183e3637db7662087ed7968a596
GabiAlmeida <gabi.almeida.souza25@gmail.com>
2019-04-17 22:08:07 -0300

    Many things

--- main.js
+++ main.js
@@ -7,5 +7,8 @@ function openSearch() {
     document.getElementById("myOverlay").style.display = "none";
   }
 
- 
+ $('.jqcloud-word').on("hover",function(){
+   $('.img_bg').addClass("img2");
+   $('.img_bg.img2').removeClass("img_bg");
+ });
 
