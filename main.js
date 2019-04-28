846b2823b539c45794992016c2bce2d3e3e8fc20
smtjunior <jniorsandro@gmail.com>
2019-04-28 20:16:17 -0300

    inicio textos

--- main.js
+++ main.js
@@ -1,85 +1,138 @@
 var modais = {
   tips: [
-  {
- "title": "Exames de Rotina",
- "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consequat, ligula ac pellentesque faucibus, risus lorem eleifend turpis, id egestas massa tortor at urna. Vivamus at sodales elit. Donec vestibulum ex nulla, a hendrerit erat elementum sit amet. Sed ac cursus nisi, nec porttitor diam. Nullam malesuada rhoncus sapien quis gravida. Sed eget massa suscipit, varius turpis nec, efficitur metus. Morbi aliquet venenatis nibh, non varius risus sodales et. Aenean eget felis tincidunt dui condimentum aliquam. Vivamus ultricies rutrum dolor, in viverra nisi ultricies hendrerit. Aenean fringilla porta dolor at fermentum. Fusce fermentum nibh a mauris sollicitudin egestas. Donec sed sapien mattis risus porta imperdiet. Nullam rhoncus magna eget nibh gravida, suscipit cursus dui volutpat. Aenean pretium augue interdum libero malesuada egestas. Nulla facilisi. Pellentesque facilisis tristique lectus vel ornare. Vestibulum vel urna lectus. Cras id viverra nisl. Curabitur vel tristique metus, non blandit mauris. Proin viverra vehicula turpis, at malesuada sapien pretium nec. Nam in tortor ac dolor feugiat porttitor. In consequat mollis nulla porttitor tempor. Duis vel turpis turpis. Mauris vitae sagittis erat. Nam auctor nulla vitae imperdiet commodo. Suspendisse vulputate ex sit amet quam dignissim, nec vulputate tellus accumsan. Ut id sapien placerat, interdum risus a, scelerisque.",
+  { // 0
+   "title": "Exames de Rotina",
+   "text": `exame de mama: é muito importante na prevenção do câncer de mama pode ser feito em casa pela própria mulher e é apenas para precaução já que podem ser percebidos nódulos através deste exame atentando a mulher para uma consulta com médico. o auto exame pode ser feito no banho, em frente ao espelho e deitada.
+   toque vaginal: é fundamental para diagnosticar endometriose e doença pélvica o exame é feito pela inserção de dois dedos do ginecologista na vagina e com a compressão do abdômen podendo assim sentir e avaliar o colo do útero, útero, trompas e ovários.
+   papanicolau: é usado para detectar inflamações, HPV, câncer do colo do útero e displasias, o exame é feito com a coleta de celuas e secreções vafinais que são analisadas em laboratorio.
+   laparoscopia: é indicado para o diagnóstico e tratamento de infertilidade e endometriose, se faz com a realização de cirurgias com a inserção de pequenas cânulas de metal no abdômen
+   vulvoscopia: é indicado para mulheres que tiveram resultado anormal no papanicolau. o exame é feito com o colposcópio onde examinam a vulva com produtos que realçam as áreas a serem examinadas.
+   colposcopia: este exame é realizado com colposcópio que aumenta a visualização de 10 a 40x da vagina e do colo de útero, é importante frisar que este exame é feito fora do período menstrual e a mulher tem que ter abstinência sexual de 48 horas e evitar exercícios intensos.
+   mamografia: é o melhor método para detecção precoce de câncer de mama mesmo antes de os nódulos não serem palpáveis, é recomendado para mulheres acima de 40 anos;
+   `,
+   "image": "gravidez.jpg"
+ },
+ {  // 1
+   "title": "Infertilidade",
+   "text": `o que é: é a dificuldade em se reproduzir, o diagnóstico é dado por um médico especializado e pode demorar para ser diagnosticado pois as chances de um casal fértil engravidar são de 15 a 25% mês sendo estas chances aumentadas drasticamente ao longo de um ano.Existem dois tipos de infertilidade, a primária que é quando a mulher nunca teve uma gravidez e a secundária: quando a mulher já teve uma gravidez anterior estas podem ser caracterizadas como inférteis pois por diversos motivos engravidam mas não conseguem manter a gestação até o final.
+   possíveis causas: podem ser variadas, inicialmente o casal com dificuldade em ter um bebê deve se submeter a exames para avaliar a capacidade reprodutora de cada um. De acordo com a Sociedade Americana da Medicina Reprodutiva, conseguimos mapear as principais causas:
+   <br><br>- Idade: conforme dito anteriormente, com o avançar da idade a quantidade produção de óvulos da mulher diminui;
+   <br><br>- DST’s
+   <br><br>- Peso: a obesidade e a magreza extrema podem afetar a capacidade de reprodução feminina;
+   <br><br>- Tabagismo: o uso de cigarro reduz a fertilidade, este aspecto afeta mulheres e homens.
+   <br><br>- Tratamentos: primeiramente é importante destacar que após os 35 anos de idade a fertilidade da mulher diminui em um terço, logo mesmo com tratamentos tecnológicos para fertilização ainda sim pode haver dificuldade em alcançar a gestação. 
+   `,
+   "image": "infertilidade-feminina.jpg"
+ },
+ {  // 2
+   "title": "Doenças Femininas",
+   "text": `HPV: é uma infecção que pode ser transmitida por prática de atos sexuais não devidamente protegidos (sem camisinha) assim como pelo contato das mucosas internas infectadas em caso de parto onde a mãe possua a infecção. esta pode ser controlada e possui vacinas e campanhas de prevenção feitas pelo governo, porém não existe cura; assim como pode ou não ter sintomas visuais. por vezes se manifesta em forma de verrugas sangramentos e queimadura nas partes íntimas mas pode se manifestar com lesões internas, por exemplo no colo do útero.Existem exames para identificar sua presença.
+   <br><br>- Candidíase: é uma infecção ocasionada do fungo Candida albicans que já está na flora vaginal feminina e sua manifestação no geral ocorre por um desequilíbrio da flora vaginal e este pode ter alguns facilitadores, por exemplo: Antibióticos, Gravidez, Diabetes e outros. logo a possibilidade de ser passado por sexo desprotegido é bem pequena.
+   <br><br>- Endometriose:as causas desta doença ainda são estudadas, mas há teorias estudadas que estimam que esta doença se faz com a descamação do endométrio expelido no período menstrual onde parte do sangue é migrado e cai nos ovários ou cavidade abdominal causando a lesão endométrica. A possibilidade de uma mulher possuir a doença aumenta consideravelmente caso alguem da familia possua;
+   <br><br>- Câncer de mama: é um tumor maligno formado pelo crescimento desordenado de nódulos na mama. é o câncer mais comum nas brasileiras e que mais causa mortes. não há causas específicas para sua aparição mas existem fatores de risco, dentre eles podemos citar: Ser mulher, Idade (mulheres acima dos 50 anos correm mais risco), Histórico familiar, não ter filhos ou ter depois dos 30 anos,Elevado consumo de álcool.
+   <br><br>- Câncer de colo de útero:é uma lesão invasiva intrauterina e assintomática podendo demorar de 10 a 20 anos para ser desenvolvida no organismo, é muito ocasionada pelo HPV, tabagismo, sexo desprotegido e multiparidade. esta doença se não tratada pode evoluir para um tumor maligno chamado carcinoma.
+   <br><br>- Mioma:são tumores benignos do útero que consistem no distúrbio hormonal cansando o enovelamento das fibras musculares causando nódulos no útero, o hormônio estrogênio é principal causador desta doença, logo as maiores incidências ocorrem na fase produtiva até a menopausa, as mulheres mais afetadas são negras e possuem 2 fatores que possuem maior propensão para doença: obesidade e não possuir filhos (nuliparidade);
+   <br><br>- Ovários policísticos: é um distúrbio que provoca formação de cistos nos ovários e por haver alteração dos níveis hormonais fazem com que estes aumentem. assim como endometriose, sua causa ainda é estudada mas possui estudos da mesma com ligação genética e resistência a insulina produzida no organismo, esta provocaria o desequilíbrio hormonal.
+   `,
+   "image": "gravidez.jpg"
+ },
+ {  // 3
+  "title": "DIU",
+  "text": `é um dispositivo intrauterino que deve ser inserido e indicado por um médico, este método é bastante vantajoso em relação a eficácia da proteção. seu tempo de troca pode variar entre 5 e 10 anos dependendo do modelo a ser indicado pelo médico, existem 3 mais conhecidos.
+  <br><br>- Mirena: este é um dispositivo hormonal, ou seja, o mesmo é inserido e possui uma tecnologia que a cada 24 horas solta hormônio dentro do corpo da mulher, suas vantagens se fazem em tornar o fluxo menstrual menor e possuir uma quantidade bem menor de hormônios do que é encontrado em pílulas.
+  <br><br>- Cobre: este é o dispositivo tradicional, feito de cobre. atualmente não é muito utilizado por aumentar em até 3 dias o fluxo menstrual assim como também pode acarretar alguns desconfortos como dores de cólica maiores; é importante frisar que nem todas as mulheres sentem estes desconfortos ao colocar o diu de cobre, as desvantagens se dão pela fragmentação do cobre no organismo, que ocorre com menor frequência..
+  <br><br>- Prata: a diferença deste dispositivo para o anterior é que as pontas do diu, que “matam” o espermatozóide, são feitas de prata no intuito de diminuir a fragmentação do cobre no organismo, esta fragmentação é responsável por algumas das desvantagens citadas acima.`,
+  "image": "gravidez.jpg"
+},
+{ // 4
+  "title": "Tratamentos para Infertilidade",
+  "text": `- Fertilização in vitro: é a fecundação natural do óvulo pelo espermatozóide feita fora do corpo em um meio de cultura, onde os embriões serão implantados no útero da mulher. este processo pode demorar por volta de duas semanas.
+  <br><br>- Inseminação: é a inserção do espermatozóide diretamente no útero em um período de 36 horas após a ovulação, esta técnica é pouco invasiva já que o desenvolvimento do embrião ocorre inteiramente dentro do organismo feminino
+  <br><br>- Indução de ovulação: é o estímulo de produção de óvulos durante o período fértil da mulher, o processo é feito com o aumento da quantidade de FSH que incita o crescimento e maturação dos óvulos.
+  <br><br- Transferência intratubária de gametas: é a inserção de gametas e óvulos dentro das tubas uterinas onde se tudo ocorrer bem, os espermatozóides irão penetrar nos óvulos e formar um embrião, esta técnica possui concepção do bebê a todo momento dentro da mulher.
+  `,
+  "image": "gravidez.jpg"
+},
+{ // 5
+ "title": "Outra Coisa",
+ "text": "Texto da outra coisa",
  "image": "gravidez.jpg"
 },
-{
- "title": "Infertilidade",
- "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consequat, ligula ac pellentesque faucibus, risus lorem eleifend turpis, id egestas massa tortor at urna. Vivamus at sodales elit. Donec vestibulum ex nulla, a hendrerit erat elementum sit amet. Sed ac cursus nisi, nec porttitor diam. Nullam malesuada rhoncus sapien quis gravida. Sed eget massa suscipit, varius turpis nec, efficitur metus. Morbi aliquet venenatis nibh, non varius risus sodales et. Aenean eget felis tincidunt dui condimentum aliquam. Vivamus ultricies rutrum dolor, in viverra nisi ultricies hendrerit. Aenean fringilla porta dolor at fermentum. Fusce fermentum nibh a mauris sollicitudin egestas. Donec sed sapien mattis risus porta imperdiet. Nullam rhoncus magna eget nibh gravida, suscipit cursus dui volutpat. Aenean pretium augue interdum libero malesuada egestas. Nulla facilisi. Pellentesque facilisis tristique lectus vel ornare. Vestibulum vel urna lectus. Cras id viverra nisl. Curabitur vel tristique metus, non blandit mauris. Proin viverra vehicula turpis, at malesuada sapien pretium nec. Nam in tortor ac dolor feugiat porttitor. In consequat mollis nulla porttitor tempor. Duis vel turpis turpis. Mauris vitae sagittis erat. Nam auctor nulla vitae imperdiet commodo. Suspendisse vulputate ex sit amet quam dignissim, nec vulputate tellus accumsan. Ut id sapien placerat, interdum risus a, scelerisque.",
- "image": "infertilidade-feminina.jpg"
-},
-  {
- "title": "Doenças Femininas",
- "text": "Want people to use your product? Use it yourself",
+{ // 6
+ "title": "Gravidez",
+ "text":  `<br><br>- Exames: durante a gravidez devem ser feitos vários exames para fazer acompanhamento do desenvolvimento do bebê; um dos mais comuns a ser pedido pelo médico é o ultrassom, este verifica não somente o sexo do bebê mas também sua saúde funcional e anatomia. Abaixo especificaremos outros exames que podem ser solicitados para acompanhamento da gestante e do bebê e qual sua importância.
+ <br><br>- Determinação de grupo sanguíneo e do fator Rh: previne e evita a incompatibilidade sanguínea entre mãe e bebê, é importante pois quando o fator rh da mãe é negativo e do feto positivo, os anticorpos da mãe atacam o sangue do feto. esta discrepância pode ser tratada.
+ <br><br>- Hemograma: é importante para identificar anemia.
+ <br><br>- Glicemia de jejum: detecta a quantidade e a concentração de glicose no sangue
+ <br><br>- Exame de urina: previne infecção urinária;
+ <br><br>- Papanicolau: detecta doenças de colo de útero;
+ <br><br>- Ultrassom obstétrico: identifica cronologia da gestação e pode ser usado para descobrir doenças cromossômicas e malformações do feto e no último trimestre de gestação avalia crescimento fetal e mostra possíveis complicações, tais como excesso de peso ou desnutrição.
+ <br><br>- Ultrassom obstétrico morfológico:analisa a formação dos órgãos fetais;
+ <br><br>- Amamentação: é essencial para o desenvolvimento e fortalecimento da musculatura do bebê, oclusão dos dentes e até da fala mas também traz muitos benefícios para a mãe como o menor risco de desenvolvimento de câncer e osteoporose. O leite possui três fases, o colostro que é o primeiro leite rico em proteínas e anticorpos pois ao nascer o leite materno é a única fonte de anticorpos do bebê; o leite de transição, que é entre o colostro e o leite maduro, este possui mais calorias; e por fim o leite maduro que é produzido até o final da gestação, este tem todos os componentes necessários para o bebê e o protege de doenças infecciosas e diarreia.
+ `,
  "image": "gravidez.jpg"
 },
-{
+{ // 7
  "title": "Infertilidade",
  "text": "Font Size Idea: px at Root, rem for Components, em for Text Elements",
  "image": "gravidez.jpg"
 },
-  {
- "title": "Exames de Rotina",
- "text": "Want people to use your product? Use it yourself",
- "image": "gravidez.jpg"
+{ // 8
+
 },
-{
- "title": "Infertilidade",
- "text": "Font Size Idea: px at Root, rem for Components, em for Text Elements",
- "image": "gravidez.jpg"
+{ //9
+
 },
-  {
- "title": "Exames de Rotina",
- "text": "Want people to use your product? Use it yourself",
- "image": "gravidez.jpg"
-},
-{
- "title": "Infertilidade",
- "text": "Font Size Idea: px at Root, rem for Components, em for Text Elements",
- "image": "gravidez.jpg"
+{ // 10
+  "title": "Parto",
+  "text": `Existem dois tipos de parto, o cirúrgico e o vaginal, estes se desdobram em vários outros os quais iremos explicar logo abaixo:
+  <br><br>- Cócoras: somente pode ser realizado se o bebê estiver em posição cefálica, é um parto mais rápido já que a mulher se encontra em posição de cócoras, a força da gravidade é um auxílio e também é mais cômodo dada posição (cócoras), não há compressão de vasos sanguíneos da mãe.
+  <br><br>- Leboyer: é um parto focado no conforto do nascimento do bebê, o obstetra Leboyer dava importância a uma forma menos “violenta” possível logo os partos eram realizados em ambientes com pouca luz, silêncio, massagens no bebê e esperava o cordão umbilical para de pulsar para que a transição respiratória do bebê seja mais suave.
+  <br><br>- Na água: é feito em uma banheira de água quente focando no alívio das dores do parto já que águas quentes aliviam dores, tensão e ajuda na flexibilidade do períneo na hora do parto.
+  <br><br>- Sem dor: envolve métodos psicofiláticos que propõe técnicas de relaxamento e alívio de dores baseado no controle. técnicas de relaxamento e respiração no momento do parto. este método ainda possui a dor natural do parto, porém é importante frisar que seu foco é diminuir as dores e tensões causadas por medo. sendo assim um parto sem sofrimento.
+  <br><br>- Cesária: é um método cirúrgico mais invasivo onde a gestante não pode ser sedada por completo, logo ganha anestesia local e lhe são cortadas 7 camadas de pele para realizar a retirada do bebê, como no geral, a escolha da cesária é feita pelo fato de o bebê não estar na posição correta para o nascimento, o mesmo também não está preparado para tal, então, este tipo de parto é mais agressivo para o bebê que se sente arrancado do útero e é mais sofrido assim como também é agressivo para a gestante nos momentos de recuperação embora seja um parto muito rápido comparado aos vaginais naturais.`,
+  "image": "gravidez.jpg"
 }
 ]};
 
 function openSearch() {
-    document.getElementById("myOverlay").style.display = "block";
-  }
-  
-  function closeSearch() {
-    document.getElementById("myOverlay").style.display = "none";
-    resetBusca();
-  }
+  document.getElementById("myOverlay").style.display = "block";
+}
 
-  (function($)
+function closeSearch() {
+  document.getElementById("myOverlay").style.display = "none";
+  resetBusca();
+}
+
+(function($)
+{
+ var settings;
+ $.fn.tx3TagCloud = function(options)
  {
-   var settings;
-     $.fn.tx3TagCloud = function(options)
-     {
- 
+
        //
        // DEFAULT SETTINGS
        //
        settings = $.extend({
-         multiplier		: 1
+         multiplier   : 1
        }, options);
        main(this);
- 
+
      }
- 
+
      function main(element)
      {
        // adding style attr
        element.addClass("tx3-tag-cloud");
        addListElementFontSize(element);
      }
- 
+
      /**
       * calculates the font size on each li element 
       * according to their data-weight attribut
       */
-     function addListElementFontSize(element)
-     {
+      function addListElementFontSize(element)
+      {
        var hDataWeight = -9007199254740992;
        var lDataWeight = 9007199254740992;
        $.each(element.find("li"), function(){
@@ -99,40 +152,41 @@ function openSearch() {
          var percent = Math.abs((dataWeight - lDataWeight)/(lDataWeight - hDataWeight));
          $(this).css('font-size', (1 + (percent * settings['multiplier'])) + "em");
        });
- 
+
      }
- 
+
      function getDataWeight(element)
      {
        return parseInt($(element).attr("data-weight"));
      }
- 
+
      function logWarning(message)
      {
        console.log("[WARNING] " + Date.now() + " : " + message);
      }
- 
- }(jQuery));
-  $(document).ready(function(){
-     $("#tagcloud").tx3TagCloud({
-         multiplier: 5
-     });
- });
 
- function setaDadosModal(id) {
-    document.getElementById('title').innerHTML = modais.tips[id].title;
-    document.getElementById('text').innerHTML = modais.tips[id].text;	
-    document.getElementById('image').src += modais.tips[id].image;	
+   }(jQuery));
+$(document).ready(function(){
+ $("#tagcloud").tx3TagCloud({
+   multiplier: 5
+ });
+});
+
+function setaDadosModal(id) {
+  document.getElementById('title').innerHTML = modais.tips[id].title;
+  document.getElementById('text').innerHTML = modais.tips[id].text; 
+  resetImageModalPath();
+  document.getElementById('image').src += modais.tips[id].image;  
 }
 function resetImageModalPath(){
-  document.getElementById('image').src = "./Imagens/Modal/";	
+  document.getElementById('image').src = "./Imagens/Modal/";  
 }
 function changeBg(image)
 {
   var last=$('#background_imagem').attr('class');
   $('#background_imagem').removeClass(last);
   $('#background_imagem').addClass(image);
-   
+
 }
 
 function validaFiltro (){
@@ -165,6 +219,6 @@ $('form').submit(function(){
   var e = document.getElementById("itemSelected");
   var codeSearch = e.options[e.selectedIndex].id;
 
-   $(this).attr('action',$(this).attr('action')+ codeSearch);
-   $(this).submit();
+  $(this).attr('action',$(this).attr('action')+ codeSearch);
+  $(this).submit();
 });
