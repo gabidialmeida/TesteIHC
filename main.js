526bb3ae2346f325618480a1e9e9e79210a018a8
Gabriela Coelho <GabrielaCoelho@users.noreply.github.com>
2019-04-28 21:40:44 -0300

    texto modal e nuvem de palavras

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ var modais = {
   tips: [
   { // 0
    "title": "Exames de Rotina",
-   "text": `exame de mama: é muito importante na prevenção do câncer de mama pode ser feito em casa pela própria mulher e é apenas para precaução já que podem ser percebidos nódulos através deste exame atentando a mulher para uma consulta com médico. o auto exame pode ser feito no banho, em frente ao espelho e deitada.
-   toque vaginal: é fundamental para diagnosticar endometriose e doença pélvica o exame é feito pela inserção de dois dedos do ginecologista na vagina e com a compressão do abdômen podendo assim sentir e avaliar o colo do útero, útero, trompas e ovários.
-   papanicolau: é usado para detectar inflamações, HPV, câncer do colo do útero e displasias, o exame é feito com a coleta de celuas e secreções vafinais que são analisadas em laboratorio.
-   laparoscopia: é indicado para o diagnóstico e tratamento de infertilidade e endometriose, se faz com a realização de cirurgias com a inserção de pequenas cânulas de metal no abdômen
-   vulvoscopia: é indicado para mulheres que tiveram resultado anormal no papanicolau. o exame é feito com o colposcópio onde examinam a vulva com produtos que realçam as áreas a serem examinadas.
-   colposcopia: este exame é realizado com colposcópio que aumenta a visualização de 10 a 40x da vagina e do colo de útero, é importante frisar que este exame é feito fora do período menstrual e a mulher tem que ter abstinência sexual de 48 horas e evitar exercícios intensos.
-   mamografia: é o melhor método para detecção precoce de câncer de mama mesmo antes de os nódulos não serem palpáveis, é recomendado para mulheres acima de 40 anos;
+   "text": `<br><br>- Exame de mama: é muito importante na prevenção do câncer de mama pode ser feito em casa pela própria mulher e é apenas para precaução já que podem ser percebidos nódulos através deste exame atentando a mulher para uma consulta com médico. o auto exame pode ser feito no banho, em frente ao espelho e deitada.
+   <br><br>- Toque vaginal: é fundamental para diagnosticar endometriose e doença pélvica o exame é feito pela inserção de dois dedos do ginecologista na vagina e com a compressão do abdômen podendo assim sentir e avaliar o colo do útero, útero, trompas e ovários.
+   <br><br>- Papanicolau: é usado para detectar inflamações, HPV, câncer do colo do útero e displasias, o exame é feito com a coleta de celuas e secreções vafinais que são analisadas em laboratorio.
+   <br><br>- Laparoscopia: é indicado para o diagnóstico e tratamento de infertilidade e endometriose, se faz com a realização de cirurgias com a inserção de pequenas cânulas de metal no abdômen
+   <br><br>- Vulvoscopia: é indicado para mulheres que tiveram resultado anormal no papanicolau. o exame é feito com o colposcópio onde examinam a vulva com produtos que realçam as áreas a serem examinadas.
+   <br><br>- Colposcopia: este exame é realizado com colposcópio que aumenta a visualização de 10 a 40x da vagina e do colo de útero, é importante frisar que este exame é feito fora do período menstrual e a mulher tem que ter abstinência sexual de 48 horas e evitar exercícios intensos.
+   <br><br>- Mamografia: é o melhor método para detecção precoce de câncer de mama mesmo antes de os nódulos não serem palpáveis, é recomendado para mulheres acima de 40 anos;
    `,
-   "image": "gravidez.jpg"
+   "image": "exames_rotina.jpg"
  },
  {  // 1
    "title": "Infertilidade",
@@ -34,7 +34,7 @@ var modais = {
    <br><br>- Mioma:são tumores benignos do útero que consistem no distúrbio hormonal cansando o enovelamento das fibras musculares causando nódulos no útero, o hormônio estrogênio é principal causador desta doença, logo as maiores incidências ocorrem na fase produtiva até a menopausa, as mulheres mais afetadas são negras e possuem 2 fatores que possuem maior propensão para doença: obesidade e não possuir filhos (nuliparidade);
    <br><br>- Ovários policísticos: é um distúrbio que provoca formação de cistos nos ovários e por haver alteração dos níveis hormonais fazem com que estes aumentem. assim como endometriose, sua causa ainda é estudada mas possui estudos da mesma com ligação genética e resistência a insulina produzida no organismo, esta provocaria o desequilíbrio hormonal.
    `,
-   "image": "gravidez.jpg"
+   "image": "doenças_femininas.jpg"
  },
  {  // 3
   "title": "DIU",
@@ -42,7 +42,7 @@ var modais = {
   <br><br>- Mirena: este é um dispositivo hormonal, ou seja, o mesmo é inserido e possui uma tecnologia que a cada 24 horas solta hormônio dentro do corpo da mulher, suas vantagens se fazem em tornar o fluxo menstrual menor e possuir uma quantidade bem menor de hormônios do que é encontrado em pílulas.
   <br><br>- Cobre: este é o dispositivo tradicional, feito de cobre. atualmente não é muito utilizado por aumentar em até 3 dias o fluxo menstrual assim como também pode acarretar alguns desconfortos como dores de cólica maiores; é importante frisar que nem todas as mulheres sentem estes desconfortos ao colocar o diu de cobre, as desvantagens se dão pela fragmentação do cobre no organismo, que ocorre com menor frequência..
   <br><br>- Prata: a diferença deste dispositivo para o anterior é que as pontas do diu, que “matam” o espermatozóide, são feitas de prata no intuito de diminuir a fragmentação do cobre no organismo, esta fragmentação é responsável por algumas das desvantagens citadas acima.`,
-  "image": "gravidez.jpg"
+  "image": "diu.jpg"
 },
 { // 4
   "title": "Tratamentos para Infertilidade",
@@ -51,16 +51,18 @@ var modais = {
   <br><br>- Indução de ovulação: é o estímulo de produção de óvulos durante o período fértil da mulher, o processo é feito com o aumento da quantidade de FSH que incita o crescimento e maturação dos óvulos.
   <br><br- Transferência intratubária de gametas: é a inserção de gametas e óvulos dentro das tubas uterinas onde se tudo ocorrer bem, os espermatozóides irão penetrar nos óvulos e formar um embrião, esta técnica possui concepção do bebê a todo momento dentro da mulher.
   `,
-  "image": "gravidez.jpg"
+  "image": "tratamento_infertilidade.jpg"
 },
 { // 5
- "title": "Outra Coisa",
- "text": "Texto da outra coisa",
- "image": "gravidez.jpg"
+ "title": "Camisinha Feminina",
+ "text": `É eficiente não somente na prevenção de gravidez em períodos indesejados como também é extremamente importante para prevenção de doenças Sexualmente Transmissíveis; existem dois tipos de camisinha, masculina a qual no geral é mais utilizada e comercializada e a feminina que é um tubo de poluiretano com duas extremidades, uma fechada e outra aberta, esta possui dois anéis acoplados onde seja possível que o anel móvel da camisinha alcance o colo do útero.`,
+ "image": "camisinha_feminina.jpg"
 },
 { // 6
  "title": "Gravidez",
- "text":  `<br><br>- Exames: durante a gravidez devem ser feitos vários exames para fazer acompanhamento do desenvolvimento do bebê; um dos mais comuns a ser pedido pelo médico é o ultrassom, este verifica não somente o sexo do bebê mas também sua saúde funcional e anatomia. Abaixo especificaremos outros exames que podem ser solicitados para acompanhamento da gestante e do bebê e qual sua importância.
+ "text":  `É a resultante da fecundação do óvulo feminino pelo gameta masculino, esta fecundação habitualmente ocorre no útero e é responsável pela geração de um novo ser humano, um bebê.
+ Nesta nova fase, haverão várias mudanças no corpo da mulher a preparando para o crescimento do feto e posteriormente para o parto do bebê. Abaixo citaremos alguns exames em que os médicos obstetras solicitam para o acompanhamento da gestação da mulher.
+ <br><br>- Ultrassom, este verifica não somente o sexo do bebê mas também sua saúde funcional e anatomia. Abaixo especificaremos outros exames que podem ser solicitados para acompanhamento da gestante e do bebê e qual sua importância.
  <br><br>- Determinação de grupo sanguíneo e do fator Rh: previne e evita a incompatibilidade sanguínea entre mãe e bebê, é importante pois quando o fator rh da mãe é negativo e do feto positivo, os anticorpos da mãe atacam o sangue do feto. esta discrepância pode ser tratada.
  <br><br>- Hemograma: é importante para identificar anemia.
  <br><br>- Glicemia de jejum: detecta a quantidade e a concentração de glicose no sangue
@@ -68,20 +70,34 @@ var modais = {
  <br><br>- Papanicolau: detecta doenças de colo de útero;
  <br><br>- Ultrassom obstétrico: identifica cronologia da gestação e pode ser usado para descobrir doenças cromossômicas e malformações do feto e no último trimestre de gestação avalia crescimento fetal e mostra possíveis complicações, tais como excesso de peso ou desnutrição.
  <br><br>- Ultrassom obstétrico morfológico:analisa a formação dos órgãos fetais;
- <br><br>- Amamentação: é essencial para o desenvolvimento e fortalecimento da musculatura do bebê, oclusão dos dentes e até da fala mas também traz muitos benefícios para a mãe como o menor risco de desenvolvimento de câncer e osteoporose. O leite possui três fases, o colostro que é o primeiro leite rico em proteínas e anticorpos pois ao nascer o leite materno é a única fonte de anticorpos do bebê; o leite de transição, que é entre o colostro e o leite maduro, este possui mais calorias; e por fim o leite maduro que é produzido até o final da gestação, este tem todos os componentes necessários para o bebê e o protege de doenças infecciosas e diarreia.
  `,
  "image": "gravidez.jpg"
 },
 { // 7
- "title": "Infertilidade",
- "text": "Font Size Idea: px at Root, rem for Components, em for Text Elements",
- "image": "gravidez.jpg"
+ "title": "Anticoncepcional",
+ "text": `<br><br>- Existem 3 tipos de pílulas anticoncepcionais, estas são Monofásica, Minipílula e Multifásica. 
+<br><br>- Monofásica - é mais utilizada e possui dosagens de estrogênio e progesterona; é preciso uma pausa de 7 dias quando a cartela acaba.
+<br><br>- Minipílula - possui somente progesterona é indicada para mulheres que estão amamentando e querem evitar uma nova gravidez. deve ser tomada sem interrupção
+<br><br>- Multifásica - a dosagem e os hormônios são feitos conforme a fase do ciclo reprodutivo, é preciso fazer uma pausa de 4 dias.
+<br><br>- Injeção: possui em sua fórmula progesterona ou associação de estrogênios com doses de longa duração, já que este pode ser aplicado mensalmente ou trimestralmente
+<br><br>- Adesivo: possui a combinação de progestogênio e estrogênio, estes são liberados na corrente sanguínea por 7 dias consecutivos, sua aplicação é feita por colar o material aderente em locais como barriga, costas ou nádegas. deve ser feito um uso consecutivo por 3 semanas tendo pausa de uma semana
+`,
+ "image": "anticoncepcional.jpg"
 },
 { // 8
-
+"title": "Contracepção cirúrgica",
+"text": `São métodos conhecidos como definitivos, podem ser feitos tanto nos homens (vasectomia) quanto nas mulheres (ligadura de trompas).esta se faz em cortar as trompas femininas por onde os espermatozóides passam para encontrar os óvulos, já a vasectomia é mais segura e mais rápida pois se trata do fechamento dos canais deferentes do homem, logo o canal no escroto por onde passam os espermatozóides é cortado impedindo que os espermatozóides não cheguem aos testículos. a execução deste método não atrapalha as funções biológicas do homem apenas é um método definitivo de contraceptivo.
+`,
+"image":"laqueadura.jpg"
 },
 { //9
-
+"title": "Amamentação",
+"text": `É essencial para o desenvolvimento e fortalecimento da musculatura do bebê, oclusão dos dentes e até da fala mas também traz muitos benefícios para a mãe como o menor risco de desenvolvimento de câncer e osteoporose. O leite possui três fases:
+<br><br>- Colostro que é o primeiro leite rico em proteínas e anticorpos pois ao nascer o leite materno é a única fonte de anticorpos do bebê;
+<br><br>- Leite de transição, que é entre o colostro e o leite maduro, este possui mais calorias; 
+<br><br>- Leite maduro que é produzido até o final da gestação, este tem todos os componentes necessários para o bebê e o protege de doenças infecciosas e diarreia.
+`,
+"image":"amamentação.jpg"
 },
 { // 10
   "title": "Parto",
@@ -91,7 +107,17 @@ var modais = {
   <br><br>- Na água: é feito em uma banheira de água quente focando no alívio das dores do parto já que águas quentes aliviam dores, tensão e ajuda na flexibilidade do períneo na hora do parto.
   <br><br>- Sem dor: envolve métodos psicofiláticos que propõe técnicas de relaxamento e alívio de dores baseado no controle. técnicas de relaxamento e respiração no momento do parto. este método ainda possui a dor natural do parto, porém é importante frisar que seu foco é diminuir as dores e tensões causadas por medo. sendo assim um parto sem sofrimento.
   <br><br>- Cesária: é um método cirúrgico mais invasivo onde a gestante não pode ser sedada por completo, logo ganha anestesia local e lhe são cortadas 7 camadas de pele para realizar a retirada do bebê, como no geral, a escolha da cesária é feita pelo fato de o bebê não estar na posição correta para o nascimento, o mesmo também não está preparado para tal, então, este tipo de parto é mais agressivo para o bebê que se sente arrancado do útero e é mais sofrido assim como também é agressivo para a gestante nos momentos de recuperação embora seja um parto muito rápido comparado aos vaginais naturais.`,
-  "image": "gravidez.jpg"
+  "image": "parto.jpg"
+},
+{// 11
+  "title": "Menstruação",
+  "text":`É a perda de sangue periódica causada pela descamação das paredes internas do útero. este período faz parte de um ciclo que acontece em 4 fases
+menstruação: como dito anteriormente, é a perda de sangue periódica que pode ocorrer de 3 a 7 dias.
+fase pré-ovulatória: é o período em que o óvulo se prepara para sair do ovário e o útero prepara para receber o óvulo fecundado.
+ovulação: é a fase onde a mulher possui maiores chances de engravidar, por se tratar do período fértil. 
+fase pós-ovulatória: quando não há fecundação do óvulo, este morre, logo após acontece a menstruação.
+Dicas: para amenizar cólicas menstruais é bom que: sejam praticadas atividades físicas de forma regular e moderada pois a liberação de endorfina pode diminuir a dor. seja evitado alimentos gordurosos, estes causam contrações no útero. bolsas de água quente podem sempre se aliadas pois relaxam a musculatura amenizando o impacto das contrações do útero`,
+  "image":"menstruacao.jpg"
 }
 ]};
 
